963c2e3fb99f6c38220deae5addbcc0ecfce9309
sammyay <sammyayo600@gmail.com>
2023-03-11 21:00:53 +0100

    first commit

---
+++ assignment.py
@@ -0,0 +1,9 @@
+x =int(input("Enter the first number:"))
+y =int(input("Enter the second number:"))
+
+sum = x + y
+
+if sum > 100:
+    print("the sum is large")
+if sum <= 100:
+    print("the sum is two small")    
